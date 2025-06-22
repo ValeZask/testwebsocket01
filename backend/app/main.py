d6e9f8bb321ef7e4f4b594b4cd7c59198d8a90e7
ChynNiet <nietbekovchyngyz@gmail.com>
2025-06-22 17:01:57 +0600

    Disable CORS for testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,13 +22,13 @@ logger = logging.getLogger(__name__)
 app = FastAPI(title="GroupChat WebSocket API", version="1.0.0")
 
 # Добавляем CORS для Railway
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # В продакшене лучше указать конкретные домены
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # В продакшене лучше указать конкретные домены
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/")
 async def get(request: Request):
