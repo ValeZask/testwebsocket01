fb54e70dfe1b54eaf9a3dbe4959b2e1033251efc
ChynNiet <nietbekovchyngyz@gmail.com>
2025-06-22 19:37:07 +0600

    check ci/cd 02

--- backend/app/main.py
+++ backend/app/main.py
@@ -64,7 +64,7 @@ async def get(request: Request):
 
     <div>
         <input type="text" id="messageInput" placeholder="Введите сообщение" disabled />
-        <button id="sendButton" onclick="sendMessage()" disabled>Отправить</button>
+        <button id="sendButton" onclick="sendMessage()" disabled>Отправить как герой</button>
     </div>
 
     <div>
